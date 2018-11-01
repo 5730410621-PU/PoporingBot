b3498bf10c505ee3497619f06965938dc427acbd
peerakit umbangtalad <>
2018-11-01 17:07:36 +0700

    no message

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $str = '{"richMenuId": "richmenu-88c05ef6921ae53f8b58a25f3a65faf7"}';
-
-echo __DIR__;
+$photo ='/app/appinline_design.jpeg';
+echo "<img src='".$photo."' />";
 //echo json_encode($str);
