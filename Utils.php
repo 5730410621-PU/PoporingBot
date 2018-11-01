e2ae6a607cec96393e99e7a70e52e3de8db5c59d
peerakit umbangtalad <>
2018-11-01 15:53:38 +0700

    no message

---
+++ Utils.php
@@ -0,0 +1,167 @@
+<?php
+namespace Poporing;
+
+class Utils {
+
+    public function newRichMenu(){
+        $richMenu = [
+            'size' => [ 'width' => 2500,'height' => 1686 ],
+            "selected" => false,
+            "name" => "RichMenus",
+            "chatBarText" => "เมนู",
+            "areas" => [
+                [
+                      "bounds" => [
+                        "x" => 0,
+                        "y" => 0,
+                        "width" => 833,
+                        "height" => 843
+                  ],
+                      "action" => [
+                        "type" => "message",
+                        "text" => "Horo"
+                  ]
+                ],
+                [
+                    "bounds" => [
+                        "x" => 833,
+                        "y" => 0,
+                        "width" => 833,
+                        "height" => 843
+                    ],
+                    "action" => [
+                        "type" => "message",
+                        "text" => "Poll"
+                    ]
+                ],
+                [
+                    "bounds" => [
+                        "x" => 1666,
+                        "y" => 0,
+                        "width" => 833,
+                        "height" => 843
+                    ],
+                    "action" => [
+                        "type" => "message",
+                        "text" => "Quiz"
+                    ]
+                  ],
+                [
+                    "bounds" => [
+                        "x" => 0,
+                        "y" => 843,
+                        "width" => 833,
+                        "height" => 843
+                    ],
+                    "action" => [
+                        "type" => "message",
+                        "text" => "News"
+                    ]
+                  ],
+                [
+                    "bounds" => [
+                        "x" => 833,
+                        "y" => 843,
+                        "width" => 833,
+                        "height" => 843
+                    ],
+                    "action" => [
+                        "type" => "message",
+                        "text" => "Vr/Ar"
+                    ]
+                  ],
+                [
+                    "bounds" => [
+                        "x" => 1666,
+                        "y" => 843,
+                        "width" => 833,
+                        "height" => 843
+                    ],
+                    "action" => [
+                        "type" => "message",
+                        "text" => "Report"
+                    ]
+                  ]
+            ]
+        ];
+        return $richMenu;
+    }
+
+    function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+
+    function pushMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/push";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+
+    function createRichMenu($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/richmenu";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+        return $result;
+        //return json_decode($result,true)['richMenuId'];
+        
+    }
+
+    function setRichMenu($arrayHeader,$richMenuId){
+        $strUrl = "https://api.line.me/v2/bot/user/all/richmenu/".$richMenuId;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, " ");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+        return $result;
+    }
+
+    function getRichMenu($header){
+        $strUrl = "https://api.line.me/v2/bot/richmenu/list";
+        $ch = curl_init($strUrl);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+    
+        $result = json_decode(curl_exec($ch),true);
+        $richId = $result['richmenus'][0]['richMenuId'];
+        return $richId;
+        curl_close ($ch);
+        
+    }
+}
