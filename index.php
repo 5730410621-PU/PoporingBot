287cad578822425c73118fb8fdb989a582630309
peerakit umbangtalad <>
2018-11-01 17:16:32 +0700

    no message

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<html>
+<header><title>This is title</title></header>
+<body>
+<img src="appinline_design.jpeg">
+</body>
+</html>
+
+
 <?php
 
-$im = file_get_contents("./appinline_design.jpeg");
-header("Content-type: image/jpeg");
-echo $im;
+// $im = ("appinline_design.jpeg");
+// header("Content-type: image/jpeg");
+// echo $im;
