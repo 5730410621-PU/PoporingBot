0e7f3065c28dca83cb514b647d7226deda9292e6
peerakit umbangtalad <>
2018-11-04 13:57:32 +0700

    no message

--- Utils.php
+++ Utils.php
@@ -196,5 +196,5 @@
         $strUrl = "https://api.line.me/v2/bot/message/$imgId/content";
         $ch = "curl -v -X "." GET ".$strUrl." -o ".$imgId.".png "." -H '"."$accessHeader'";
         exec($ch,$output,$result); 
-        return "Result :: ".$result." \nCh:: ".$ch;
+        return "Result :: ".__DIR__." \nCh:: ".$ch;
     }
