8bb0eadfe22d799c644fcb3f81ed6e9c300ef0f7
peerakit umbangtalad <>
2018-11-07 12:12:51 +0700

    no message

--- liffTest/liff-starter.js
+++ liffTest/liff-starter.js
@@ -1,9 +1,17 @@
 window.onload = function (e) {
     liff.init(function (data) {
-        initializeApp(data);
+       // initializeApp(data);
+       function Redirect() {  
+            liff.openWindow({
+                url: 'https://powerful-ridge-10601.herokuapp.com/camera/',
+                external : true
+            });
+        } 
+        setTimeout('Redirect()', 1000);   
     });
 };
 
+/*
 function initializeApp(data) {
     // openWindow call
     document.getElementById('openwindowbutton').addEventListener('click', function () {
@@ -13,51 +21,17 @@ function initializeApp(data) {
         });
     });
 
-    // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
-
-    /*
-    // sendMessages call
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-    });
-
-    //get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
-
-            var profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            var img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
-    */
 }
+*/
+  
+/*
+function Redirect() 
+{  
+    window.location="http://www.google.com" ;
+} 
+document.write("You will be redirected to a new page in 5 seconds"); 
+setTimeout('Redirect()', 1000);   
+*/
 
 /*
 function toggleProfileData() {
